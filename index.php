ce992074394b1744674f366dcfda2b8835bfa409
QWSTI <yegor.l@mail.ru>
2020-02-29 00:32:41 +0300

    Добавление БД и обработка форм
    Добавлены БД с одной таблицей, обработчик формы отправки заявки, реализована отправка сообщения на email БЕЗ привязки документа.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PanTłumacz</title>
+</head>
+<body>
+<?php 
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+
+	$conn = new mysqli($servername, $username, $password);
+
+	if($conn->connect_error){
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	$sql = "CREATE DATABASE Reviews";
+
+	$conn->query($sql);
+	$conn->query("USE Reviews");
+
+	$sql = "CREATE TABLE comment(
+		id INT NOT NULL AUTO_INCREMENT,
+		name varchar (30) NOT NULL,
+		comment TEXT NOT NULL,
+		period varchar (20) NOT NULL,
+		isCheked boolean default 0,
+		PRIMARY KEY(id))";
+
+	$conn->query($sql);
+?>
+
+	<script src="js/jQuery.js"></script>
+	<!-- Все скрипты подключаем сюда -->
+</body>
+</html>
---
+++ request.php
@@ -0,0 +1,31 @@
+<?php
+if (isset($_POST['firstName']) && isset($_POST['fromLang']) && isset($_POST['toLang']) && isset($_POST['email']))
+{
+	$name = htmlentities($_POST['firstName']);
+	$fromLang = $_POST['fromLang'];
+	$toLang = $_POST['toLang'];
+	$email = htmlentities($_POST['email']);
+	$comment = htmlentities($_POST['massage']);
+
+
+	$message = "
+Имя заказчика: $name
+Язык оригинала: $fromLang
+Язык перевода: $toLang
+email: $email
+Комментарий к заказу: $comment";
+
+
+	$to = "";
+	$from = "pantlumatcz@mail.ru";
+	$subject = "Заявка на перевод!";
+	$subject = "=?utf-8?D?".base64_encode($subject)."?=";
+	$headers = "From: $from\r\nReply-to: $from\t\nContent-type: text/plain; chatset=utf-8\r\n";
+
+	if(mail($to, $subject, $message, $headers))
+		echo "success";
+
+}
+else 
+	echo "ERROR";
+?>
