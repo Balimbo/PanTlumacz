50c545ccd604063854b98b5f8ce5c25e6b4c44b9
JustAndrej2510 <andrey.pisaryk@gmail.com>
2020-03-01 13:40:03 +0300

    form

---
+++ reviews.php
@@ -0,0 +1,17 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($servername, $username, $password);
+
+conn->query("USE Reviews");
+
+$name = $_POST['name'];
+$review = $_POST['review'];
+$date = date('d.m.o G:i');
+
+$sql = "INSERT INTO reviews(name, comment, period) VALUES ($name, $review, $date)";
+
+conn->query($sql);
+ ?>
