69bed63d91612ac2c085fbe6c2e1dd8e43ce6ad6
QWSTI <yegor.l@mail.ru>
2020-03-01 17:48:26 +0300

    Замена стандартных для отправки сообщения на phpMailer
    Отправка вложений не работает, все остальное работает корректно.

--- reviews.php
+++ reviews.php
@@ -1,15 +1,15 @@
 <?php 
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username 	= "root";
+$password 	= "";
 
 $conn = new mysqli($servername, $username, $password);
 
 conn->query("USE Reviews");
 
-$name = $_POST['name'];
+$name 	= $_POST['name'];
 $review = $_POST['review'];
-$date = date('d.m.o G:i');
+$date 	= date('d.m.o G:i');
 
 $sql = "INSERT INTO reviews(name, comment, period) VALUES ($name, $review, $date)";
  
