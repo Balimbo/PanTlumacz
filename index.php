219f374a34b3bca977cde04bcc4603f99a1f0f06
JustAndrej2510 <andrey.pisaryk@gmail.com>
2020-03-28 16:45:55 +0300

    header

--- index.php
+++ index.php
@@ -3,8 +3,29 @@
 <head>
 	<meta charset="UTF-8">
 	<title>PanTłumacz</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet">
 </head>
 <body>
+	<header>
+			<div class="header_menu">
+				<div class="header_logo">PanTłumacz</div>
+				<nav class="header_nav">
+					<a class="header_nav_link" href="#">Languages</a>
+					<a class="header_nav_link" href="#">What we can?</a>
+					<a class="header_nav_link" href="#">About us</a>
+					<a class="header_nav_link" href="#">Price list</a>
+					<a class="header_nav_link" href="#">Application</a>
+					<a class="header_nav_link" href="#">Comments</a>
+				</nav>
+			</div>
+
+			<div class="header_title">
+				<h1 class="header_text">Translation Agency</h1>
+				<a class="main_button" href="#">Make a request</a>
+				
+			</div>
+	</header>
 <?php 
 	require_once 'settings.php';
 
