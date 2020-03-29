8d3548bdaa23708364f133b68b3c4ee303d5837a
bxr1nG <61472060+bxr1nG@users.noreply.github.com>
2020-03-29 16:05:21 +0300

    Main styles, LangChoice
    Добавил стандартные стили, секцию выбора языка и ее стили

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>PanTłumacz</title>
 	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/langchange.css">
 	<link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -26,6 +27,81 @@
 				
 			</div>
 	</header>
+
+	<!-- Language change -->
+
+	<section class="section">
+        <div class="container">
+
+            <div class="section__header">
+                <h3 class="section__suptitle">Lorem ipsum</h3>
+                <h2 class="section__title">Lorem ipsum dolor</h2>
+                <div class="section__text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, fuga, quaerat. Aperiam repellat atque laborum, quasi libero, alias maiores optio culpa. Vel reiciendis inventore sed nisi libero, soluta ex velit, temporibus voluptatem, deleniti illo perferendis nam nulla nostrum eum molestias quam ipsam, dolor minima quae numquam quasi! Ad, minima deleniti!</p>
+                </div>
+            </div>
+
+            <div class="card">
+
+                <div class="card__item">
+                    <div class="card__inner">
+                        <div class="card__img">
+                            <img src="images/rus.png" height="300px" width="300px">
+                        </div>
+                        <div class="card__text">Polski/Rosyjski, Rosyjski/Polski</div>
+                    </div>
+                </div>
+
+                <div class="card__item">
+                    <div class="card__inner">
+                        <div class="card__img">
+                            <img src="images/germ.png" height="300px" width="300px">
+                        </div>
+                        <div class="card__text">Polski/Niemiecki, Niemiecki/Polski</div>
+                    </div>
+                </div>
+
+                <div class="card__item">
+                    <div class="card__inner">
+                        <div class="card__img">
+                            <img src="images/fr.png" height="300px" width="300px">
+                        </div>
+                        <div class="card__text">Polski/Francuski, Francuski/Polski</div>
+                    </div>
+                </div>
+
+                <div class="card__item">
+                    <div class="card__inner">
+                        <div class="card__img">
+                            <img src="images/ukr.png" height="300px" width="300px">
+                        </div>
+                        <div class="card__text">Polski/Ukraiński, Ukraiński/Polski</div>
+                    </div>
+                </div>
+
+                <div class="card__item">
+                    <div class="card__inner">
+                        <div class="card__img">
+                            <img src="images/brit.png" height="300px" width="300px">
+                        </div>
+                        <div class="card__text">Polski/Angielski, Angielski/Polski</div>
+                    </div>
+                </div>
+
+                <div class="card__item">
+                    <div class="card__inner">
+                        <div class="card__img">
+                            <img src="images/ital.png" height="300px" width="300px">
+                        </div>
+                        <div class="card__text">Polski/Włoski, Włoski/Polski</div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
 <?php 
 	require_once 'settings.php';
 
